d843f4d3b09b3ef26bbd25bfc5b32a8f6f453eb4
HimanshuSingh27 <49246916+HimanshuSingh27@users.noreply.github.com>
2019-04-17 18:14:51 +0530

    Add files via upload

---
+++ OS Question1.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+int main() 
+{ 
+      int p;       
+	  int lim;       
+	  int total_t = 0;       
+	  int x;       
+	  int counter_c = 0;       
+	  int timeQuantum;       
+	  int turnAroundTime=0;       
+	  int waitingTime=0;       
+	  int arrivalTime[10];       
+	  int burstTime[10]; 
+      int tempBurst[10],priorityArray[10];       
+	  float avg_WaitingTime, avg_TurnAroundTime;       
+	  printf("\t\tRound Robin Scheduling algorithm with Higher number priority and preemption\t");         
+	  printf("\nEnter total  Number of Processes to be scheduled:\t");       
+	  scanf("%d", &lim); 
+      x = lim; 
+}
